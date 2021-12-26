0722fd985bd651a9f9fe8ad0159279045e6ae075
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-12-26 11:44:06 +0100

    More defaults

--- README.md
+++ README.md
@@ -9,4 +9,54 @@
 ## Inputs
 
 
+### `path` (optional)
+
+Default: `$HOME/.m2/settings.xml`
+
+```yaml
+- name: Setup settings for Maven
+  uses: k15g/action-maven-settings@edge
+  with:
+    path: .m2/settings-docker.xml
+```
+
+
+### `profile` (optional)
+
+Default: `github`
+
+```yaml
+- name: Setup settings for Maven
+  uses: k15g/action-maven-settings@edge
+  with:
+    profile: myprofile
+```
+
+
+### `repositories` (optional)
+
+Default: *empty*
+
+```yaml
+- name: Setup settings for Maven
+  uses: k15g/action-maven-settings@edge
+  with:
+    repositories: |
+      myrepo:
+        url: https://repo.example.com/maven2
+```
+
+
 ### `servers` (optional)
+
+Default: *empty*
+
+```yaml
+- name: Setup settings for Maven
+  uses: k15g/action-maven-settings@edge
+  with:
+    servers: |
+      central:
+        username: myuser
+        password: mypass
+```
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,18 +5,27 @@ import merge = require('lodash.merge')
 import { initiateDocument } from './model'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as os from 'os'
 
 // Default repositories
-var default_reposetories = {
+const default_reposetories = {
     central: {
         url: 'https://repo1.maven.org/maven2'
     }
 }
 
-// Initiate document
-const doc = initiateDocument()
+// Default servers
+const default_servers = {
+    github: {
+        username: '${env.GITHUB_ACTOR}',
+        password: '${env.GITHUB_TOKEN}'
+    }
+}
 
 async function run() {
+    // Initiate document
+    const doc = initiateDocument()
+
     // Load profile identifier
     const profile = core.getInput('profile', { required: false }) || 'github'
     doc.settings.activeProfiles.activeProfile.push(profile)
@@ -30,14 +39,15 @@ async function run() {
     }
 
     // Load servers
-    const servers = yaml.load(core.getInput('servers', { required: false })) || {}
+    const servers = merge(default_servers, yaml.load(core.getInput('servers', { required: false })) || {})
 
     for (const [id, value] of Object.entries(servers)) {
         doc.settings.servers['server'].push({ id, ...value })
     }
 
     // Write to settings file
-    const path_location = core.getInput('path', { required: false }) || '.m2/settings.xml'
+    const path_location = core.getInput('path', { required: false }) || `${os.homedir}/.m2/settings.xml`
+    console.log(path_location)
     fs.mkdirSync(path.dirname(path_location), { recursive: true })
     fs.writeFileSync(path_location, create(doc).end({ prettyPrint: true }))
 }
