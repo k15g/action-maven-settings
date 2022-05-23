81d6eccda9776fc23e6f61216f57cf2026a1a019
Erlend Klakegg Bergheim <erlend@klakegg.net>
2022-05-23 21:59:09 +0200

    Deleting both settings file and the parent folder.

--- src/ts/cleanup.ts
+++ src/ts/cleanup.ts
@@ -1,11 +1,15 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
 import * as os from 'os'
+import * as path from 'path'
 
 async function run() {
     // Delete settings file
     const path_location = core.getInput('path', { required: false }) || `${os.homedir}/.m2/settings.xml`
-    fs.unlinkSync(path_location)
+    // fs.unlinkSync(path_location)
+
+    // Delete folder
+    fs.rmSync(path.dirname(path_location), { recursive: true, force: true });
 }
 
 run()
